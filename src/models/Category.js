e99df4688d3fabd3cf98591b05657a492b81bf06
Conor Kelly <conorjkelly96@gmail.com>
2021-12-15 18:50:03 +0000

    category model fixes to ID field

--- src/models/Category.js
+++ src/models/Category.js
@@ -4,6 +4,13 @@ const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
+// code: 'ER_BAD_NULL_ERROR',
+//     errno: 1048,
+//     sqlState: '23000',
+//     sqlMessage: "Column 'id' cannot be null",
+//     sql: 'INSERT INTO `category` (`id`) VALUES (NULL),(NULL),(NULL),(NULL),(NULL);',
+//     parameters: undefined
+
 Category.init(
   {
     // define columns
@@ -11,11 +18,11 @@ Category.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: false,
+      autoIncrement: true,
     },
     categoryName: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
     },
   },
   {
